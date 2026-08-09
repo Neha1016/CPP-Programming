5a6182051c90e435d701cc30d929b21c67e1f274
Neha1016 <nehachouhan1016@gmail.com>
2026-08-09 14:25:40 +0530

    Added Basic Syntax C++ programs

---
+++ Basic_Syntax/Variables.cpp
@@ -0,0 +1,26 @@
+
+// Variables 
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int age = 20 ;               
+    float marks = 67.4 ;          
+    char grade = 'A';
+    string name = "Shiv" ;
+    double price = 99.876 ;
+    bool pass = true ;
+
+    cout << age << endl ;
+    cout << marks << endl ;
+    cout << grade << endl ;
+    cout << name << endl ;
+    cout << price << endl ;
+    cout << pass << endl ;
+
+    return 0;
+}
+
+// age , marks , grade , name , price all are variables 
