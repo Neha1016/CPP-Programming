dbd9f9c1df53a52f890858e2153d815201d5e748
Neha1016 <nehachouhan1016@gmail.com>
2026-08-19 19:15:01 +0530

    Add String code

---
+++ Strings/String.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    // Declare and intialize a string
+    string name;
+
+    // Take a single word as input
+    cout << "Enter your name:";
+    cin >> name;
+
+    cout << "Your name is :" << name;
+
+    return 0;
+}
+
