2fa35e5c016abc7b455d3060e8a9c3ac488b6617
Neha1016 <nehachouhan1016@gmail.com>
2026-08-19 19:04:59 +0530

    Add Strings in Code

---
+++ Strings/Declaration.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    // Declare and intialize a string
+    string name = "Shiv";
+
+    cout << name;
+
+    return 0;
+}
+
